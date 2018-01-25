4ad91c5fd2473ce85c615e23dfd2247d5e9faa64
Tim Albert <talbert1209@gmail.com>
2018-01-25 14:46:20 -0600

    initial commit

---
+++ src/minesweeper.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+
+const blankLine = '  |   |  ';
+const guessLine = '1 |   |  ';
+const bombLine  = '  | B |  ';
+
+console.log('This is what a blank line will look like');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+console.log('This is what a board with a guess and bomb on it would look like');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
